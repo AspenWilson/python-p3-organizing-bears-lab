257d092030761b8be8f7082a222bcd76c337b719
Aspen Wilson <aspen.schell@gmail.com>
2023-05-25 17:46:08 -0700

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "Red", "Nice", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Orange", "Energetic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "M", "Yellow", "Passive agressive", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 4, "F", "Green", "Militaristic", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "M", "Blue", "Basic", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "F", "Purple", "Hates Christmas", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "M", "Indigo", "Confused", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "F", "Unicorn", "Magical", 1);
